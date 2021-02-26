3c85506e94dda50b8e08de11a0f2dc3e034c80c4
MIHUIYI689 <MIHUIYI689@pingan.com.cn>
2021-02-26 14:01:22 +0800

    update

---
+++ leetcode算法练习/数据结构/295.数据流的中位数.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: MIHUIYI689
+ * @Date: 2021-02-26 11:51:04
+ * @LastEditors: MIHUIYI689
+ * @LastEditTime: 2021-02-26 14:01:15
+ * @Description: File description
+ * @FilePath: /my-fucking-algorithm/leetcode算法练习/数据结构/295.数据流的中位数.js
+ */
+/**
+ * initialize your data structure here.
+ */
+var MedianFinder = function() {
+    
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {
+
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
+var obj = new MedianFinder()
+obj.addNum(1)
+obj.addNum(2)
+obj.findMedian()
+obj.addNum(3) 
+obj.findMedian()
